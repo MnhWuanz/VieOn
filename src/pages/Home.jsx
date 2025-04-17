6cac14537e47a0ef832ae26e3744f80a45cf395f
duchai922001 <duchai9201@gmail.com>
2025-04-17 23:10:40 +0700

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,36 @@
-import React from "react";
-import Mcard from "../components/m-carc";
+import React, { useEffect, useState } from "react";
 import dataFilm from "../json/film-orignal.json";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "../styles/home.css";
-
+import ShowFilm from "../components/show-film";
+import dataFilmTest from "../json/film-new.json";
 const Home = () => {
+  const functionConvertData = () => {
+    const convertData = dataFilmTest.map((item) => ({
+      topic: item.topic,
+      data: item,
+    }));
+    const filterData = convertData.filter((item) => item.topic);
+    const data = filterData.reduce((acc, cur) => {
+      const { topic, data } = cur;
+      if (!acc[topic]) {
+        acc[topic] = { topic, data: [] };
+      }
+      acc[topic].data.push(data);
+      return acc;
+    }, {});
+    console.log({ data });
+  };
+  const [arrayFilm, setArrayFilm] = useState([]);
+  useEffect(() => {
+    setArrayFilm(dataFilm);
+    functionConvertData();
+  }, []);
   return (
-    <div style={{ padding: "24px" }}>
-      <Swiper
-        spaceBetween={12} // Khoảng cách giữa các phần tử
-        slidesPerView={6} // Hiển thị 6 phần tử mỗi lần
-        navigation // Điều hướng next/prev
-        pagination={{ clickable: true }} // Pagination cho phép bấm vào các điểm chỉ số trang
-        breakpoints={{
-          1024: {
-            slidesPerView: 6, // Ở độ phân giải lớn hơn 1024px, hiển thị 3 phần tử
-          },
-          768: {
-            slidesPerView: 4, // Ở độ phân giải từ 768px trở xuống, hiển thị 2 phần tử
-          },
-          480: {
-            slidesPerView: 1, // Ở độ phân giải từ 480px trở xuống, hiển thị 1 phần tử
-          },
-        }}
-      >
-        {dataFilm.map((item) => (
-          <SwiperSlide key={item.id} style={{ height: "300px" }}>
-            <Mcard
-              index={item.id}
-              year={item.year}
-              duration={item.duration}
-              quality={item.quality}
-              vip={item.vip}
-              id={item.id}
-              video={item.video}
-              top={item.top}
-              image={item.image}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <>
+      {arrayFilm.map((item) => {
+        return <ShowFilm title={item.title} dataFilm={item.arrayFilm} />;
+      })}
+    </>
   );
 };
 
